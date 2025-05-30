f7050f17f0e1d736f50209e2a8d13f3d2212de98
Fubanga01 <102781753+Fubanga01@users.noreply.github.com>
2025-05-30 10:55:12 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import RangelAI from "./RangelAI";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RangelAI from './RangelAI.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RangelAI />
   </React.StrictMode>
